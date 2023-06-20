ba186db043d96246b101aa4cf3f4887a231cb50d
karthik <shrirajkulal20018@gmail.com>
2023-06-19 21:30:04 -0930

    first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+import {Link} from 'react-router-dom'; 
+
+function Navbar() {
+  return (
+   <>
+   <nav className='navbar'>
+    <div className='navbar-container'>
+    <link to="/"  className='navbar-logo'>
+        RTGW <i class="fa-solid fa-house"></i>
+    </link>
+
+    </div>
+
+
+   </nav>
+   
+   </>
+  );
+}
+
+export default Navbar;
